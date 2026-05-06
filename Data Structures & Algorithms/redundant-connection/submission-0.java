b93113092818201e4fd59fbc0034b9afeb07a67e
Vipin Sharma <sharma.vipin1608@gmail.com>
2026-05-06 15:05:42 -0700

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+
+        int[] parent = new int[n + 1];
+        int[] rank = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            int pu = find(parent, u);
+            int pv = find(parent, v);
+
+            if (pu == pv) {
+                result.add(edge);
+            } else {
+                if (rank[pu] > rank[pv]) {
+                    parent[pv] = pu;
+                    rank[pu] += rank[pv];
+                } else {
+                    parent[pu] = pv;
+                    rank[pv] += rank[pu];
+                }
+            }
+        }
+
+        return result.get(0);
+    }
+
+    private int find(int[] parent, int node) {
+        if (parent[node] == node) return node;
+        return parent[node] = find(parent, parent[node]);
+    }
+}
